4a54f9414a74872500522ef2f9229cc6d5b9815f
Rémi Hernandez <remi.hernandez@etu.u-bordeaux.fr>
2017-11-06 19:48:03 +0100

    codes

--- codes/main.f90
+++ codes/main.f90
@@ -7,7 +7,7 @@ program main
   character(25) :: output = 'output.dat'
   
   !type(matrice) :: A
-  real*8, dimension(:), allocatable :: u , unext , uprec
+  real*8, dimension(:), allocatable :: u , unext , uprev
   integer :: i,j,k,time_to_save,n,nb_iter
   real*8 :: dx,dt,tmax,xmin,xmax,a
 
@@ -27,49 +27,52 @@ program main
   nb_iter = ceiling(tmax/dt)
   print*, 'dx / dt', dx , '   /   ', dt
   print*, 'T =',tmax, ' / ', nb_iter , ' iterations'
-  allocate(uprec(0:N),unext(0:N),u(0:N))
+  allocate(uprev(0:N),unext(0:N),u(0:N))
   a = 2*dt/dx/dx
 
   open(11,file=trim(output),action='write',status='unknown')
   
-  uprec = 0.
+  uprev = 0.
   do i = ceiling(N/2.)-10, ceiling(N/2.)+10,1
-     uprec(i) = 100.
+     uprev(i) = 100.
   enddo
 
+!  write(11,'(3E22.15)') 0. , xmin , uprev(0)
   do i = 0,N
-     write(11,'(3E22.15)') 0. , real(i*dx) , uprec(i)
+     write(11,'(3E22.15)') 0., real(i*dx) , uprev(i)
   enddo
-  write(11,*)
+  
   write(11,*)
   
-  u(0) = uprec(0) + a*(uprec(1)-uprec(0))
-  u(n) = uprec(n) + a*(uprev(N)-uprev(N-1))
+  u(0) = uprev(0) + a*(uprev(1)-uprev(0))
+  u(n) = uprev(n) + a*(uprev(N)-uprev(N-1))
   do i = 1,N-1
-     u(i) = uprec(i) + (dt/dx/dx)*(uprec(i+1)-2*uprec(i)+uprec(i-1))
+     u(i) = uprev(i) + (dt/dx/dx)*(uprev(i+1)-2*uprev(i)+uprev(i-1))
   enddo
 
-  do i = 0,N
-     write(11,'(3E22.15)') dt , real(i*dx) , u(i)
-  enddo
-  write(11,*)
-  write(11,*)
+  ! do i = 0,N
+  !    write(11,'(2E22.15)')  real(i*dx) , u(i)
+  ! enddo
+  ! write(11,'(3E22.15)') dt, xmax , u(N)
+  ! write(11,*)
   
   do k = 1, nb_iter-1
 
-     unext(0) = uprec(0) + 2*a*(u(1)-u(0))
-     unext(N) = uprec(N) + 2*a*(u(N-1)-u(N))
+     unext(0) = uprev(0) + 2*a*(u(1)-u(0))
+     unext(N) = uprev(N) + 2*a*(u(N-1)-u(N))
      do i = 1,N-1
 
-        unext(i) = uprec(i)*(1-a)/(1+a) + (a/(1+a))*(u(i-1)+u(i+1))
+        unext(i) = uprev(i)*(1-a)/(1+a) + (a/(1+a))*(u(i-1)+u(i+1))
 
      enddo
-
+     
      if (mod(k,time_to_save) == 0) then
+
+        !write(11,*)  real(k*dt) , xmin , unext(0)
         do i = 0,N
            write(11,'(3E22.15)') real(k*dt) , real(i*dx) , unext(i)
         enddo
-        write(11,*)
+        
         write(11,*)
         
      endif
